7d04dca65cce585e677a7668cbbecbeeb23e5452
yukilun <102889935+yukilun@users.noreply.github.com>
2023-01-26 04:14:07 -0800

    test body-scroll-lock 2

--- client/src/components/home_components/summary_components/ShareExpenses.js
+++ client/src/components/home_components/summary_components/ShareExpenses.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { BiChevronDown, BiChevronRight } from 'react-icons/bi'
 import { useOutletContext, useNavigate } from 'react-router-dom';
 import useFetch from '../../../hooks/fetch.hook';
@@ -10,6 +10,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 import { IoClose } from 'react-icons/io5';
 import { shareExpenses } from '../../../helper/homeHelper';
 import { toast } from 'react-hot-toast';
+import { disableBodyScroll } from 'body-scroll-lock';
 
 export default function ShareExpenses() {
 
@@ -21,6 +22,11 @@ export default function ShareExpenses() {
   const [expensePerPerson, setExpensePerPerson] = useState('');
   const [settleDebtsArr, setSettleDebtsArr] = useState([]);
   const [isOpenPopup, setOpenPopup] = useState(false);
+  const scrollableDiv = useRef(null);
+
+  useEffect(()=> {
+    disableBodyScroll(scrollableDiv.current);
+  }, []);
 
   useEffect(() => {
     if (apiData && shareExpensesInfo && apiData.members.length > 0) {
@@ -105,7 +111,7 @@ export default function ShareExpenses() {
         </div>
       }
 
-      <div className='h-full overflow-y-auto px-4 text-gray-600 lg:w-full lg:flex lg:gap-8'>
+      <div className='h-full overflow-y-auto px-4 text-gray-600 lg:w-full lg:flex lg:gap-8' ref={scrollableDiv}>
 
         {/* settle debts */}
         {shareExpensesInfo?.totalExpenses !== 0 && (
